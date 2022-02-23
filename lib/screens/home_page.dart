4dac6feee1ab29c024e5af4166a4a347aabb3c23
Asad Yousuf <asadyousuf92@gmail.com>
2022-02-24 00:02:30 +0530

    day 11 done

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(15, (index) => CatalogModel.items[0]);
+
     final int days = 30;
     final String name = "Flutter";
 
@@ -13,9 +17,13 @@ class HomePage extends StatelessWidget {
         "Catalog App",
         style: TextStyle(color: Colors.black),
       )),
-      body: Center(
-        child: Container(
-          child: Text(" Welcome to $days days of development using $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return itemWidget(item: dummyList[index]);
+          },
         ),
       ),
       drawer: MyDrawer(),
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -12,3 +12,4 @@ class Mythemes {
         brightness: Brightness.dark,
       );
 }
+ 
