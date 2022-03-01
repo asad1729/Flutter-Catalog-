697305744b1c2481c68b76eeb8609ea7d299c635
Asad Yousuf <asadyousuf92@gmail.com>
2022-03-01 11:36:44 +0530

    day 17 done

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:velocity_x/src/extensions/context_ext.dart';
+import 'package:velocity_x/src/extensions/num_ext.dart';
 import 'package:velocity_x/src/extensions/string_ext.dart';
+import 'package:velocity_x/src/flutter/padding.dart';
+import 'package:velocity_x/src/flutter/sizedbox.dart';
+import 'package:velocity_x/src/flutter/widgets.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -12,6 +18,62 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.black.xl2.make(),
+      ),
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl5.center.color(context.theme.accentColor).make(),
+          30.widthBox,
+          TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(context.theme.buttonColor)),
+                  child: "Buy".text.xl3.color(Colors.black).make())
+              .w32(context),
+        ],
+      ),
+    );
+  }
+}
+
+
+class _CartList extends StatefulWidget {
+  _CartList({Key? key}) : super(key: key);
+
+  @override
+  State<_CartList> createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context,index)=>ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(icon : Icon(Icons.remove_circle_outline), 
+        onPressed: () {  },
+        ),
+        title: "Item 1".text.make(),
+
+
       ),
     );
   }
--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -1,69 +1,81 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/screens/home_details_page.dart';
-import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
+
 import 'catalog_image.dart';
 
 class CatalogList extends StatelessWidget {
-  const CatalogList({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: CatalogModel.items.length,
-        itemBuilder: (context, index) {
-          final catalog = CatalogModel.getByPosition(index);
-          return InkWell(
-              onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => homeDetailPage(catalog: catalog),
-                  )));
-          child:
-          CatalogItem(catalog: catalog);
-        });
+      shrinkWrap: true,
+      itemCount: CatalogModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.getByPosition(index);
+        return InkWell(
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => homeDetailPage(catalog: catalog),
+            ),
+          ),
+          child: CatalogItem(catalog: catalog),
+        );
+      },
+    );
   }
 }
 
 class CatalogItem extends StatelessWidget {
   final Item catalog;
 
-  const CatalogItem({Key? key, required this.catalog}) : super(key: key);
+  const CatalogItem({Key? key, required this.catalog})
+      // ignore: unnecessary_null_comparison
+      : assert(catalog != null),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return VxBox(
-        child: Row(
-      children: [
-        Hero(
+      child: Row(
+        children: [
+          Hero(
             tag: Key(catalog.id.toString()),
-            child: CatalogImage(image: catalog.image, catalog: catalog)),
-        Expanded(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            catalog.name.text.lg.color(Mythemes.darkBluish).bold.make(),
-            catalog.desc.text.light.make(),
-            10.heightBox,
-            ButtonBar(
+            child: CatalogImage(
+              image: catalog.image,
+              catalog: catalog,
+            ),
+          ),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              catalog.name.text.lg.color(context.accentColor).bold.make(),
+              catalog.desc.text.light.make(),
+              10.heightBox,
+              ButtonBar(
                 alignment: MainAxisAlignment.spaceBetween,
                 buttonPadding: EdgeInsets.zero,
                 children: [
                   "\$${catalog.price}".text.bold.xl.make(),
                   ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Mythemes.darkBluish),
-                          shape: MaterialStateProperty.all(StadiumBorder())),
-                      child: "Buy".text.make())
-                ]).pOnly(right: 8)
-          ],
-        ))
-      ],
-    )).white.rounded.square(140).make().py16().w40(context);
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            context.theme.buttonColor),
+                        shape: MaterialStateProperty.all(
+                          StadiumBorder(),
+                        )),
+                    child: "Add to cart".text.make(),
+                  )
+                ],
+              ).pOnly(right: 8.0)
+            ],
+          ))
+        ],
+      ),
+    ).color(context.cardColor).rounded.square(150).make().py16();
   }
 }
